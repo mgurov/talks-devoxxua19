8c6165bb288cfcbde65806a4f72fe4a8d12426fc
Mykola Gurov <mgurov@bol.com>
2019-10-31 18:05:44 +0200

    Domain model: order segmentation

--- src/main/kotlin/com/github/mgurov/devoxxua19/PurchaseOrderDomain.kt
+++ src/main/kotlin/com/github/mgurov/devoxxua19/PurchaseOrderDomain.kt
@@ -1,7 +1,20 @@
 package com.github.mgurov.devoxxua19
 
+import java.time.LocalDate
+
 data class PurchaseOrder(
     val product: String,
     val quantity: Int,
-    val buyer: String
+    val buyer: String,
+    val segments: List<Segment>
 )
+
+data class Segment(
+    val quantity: Int,
+    val status: SegmentStatus,
+    val date: LocalDate
+)
+
+enum class SegmentStatus {
+    NEW, CONFIRMED, DELIVERED, CANCELLED
+}
--- src/test/kotlin/com/github/mgurov/devoxxua19/BusinessLogicTest.kt
+++ src/test/kotlin/com/github/mgurov/devoxxua19/BusinessLogicTest.kt
@@ -2,28 +2,9 @@ package com.github.mgurov.devoxxua19
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 class BusinessLogicTest {
-    @Test
-    fun `should select by buyer`() {
-        val expected1 = PurchaseOrder("product A", 1, "me")
-        val expected2 = PurchaseOrder("product B", 2, "me")
-        val unexpected1 = PurchaseOrder("product A", 2, "someone else")
-
-        //when
-        val actual = BusinessLogic.selectByBuyer(
-            listOf(
-                expected1,
-                expected2,
-                unexpected1
-            ), "me")
-
-        //then
-        assertThat(actual).containsExactlyInAnyOrder(
-            expected1,
-            expected2
-        )
-    }
 
     @Test
     fun `should select by buyer - helper functions`() {
@@ -46,15 +27,17 @@ class BusinessLogicTest {
         )
     }
 
-    fun aPurchaseOrder(
-        product: String = "whatever product",
-        quantity: Int = 1,
-        buyer: String = "me, handsome"
-    ): PurchaseOrder {
-        return PurchaseOrder(
-            product = product,
-            quantity = quantity,
-            buyer = buyer
-        )
-    }
+}
+
+private fun aPurchaseOrder(
+    product: String = "whatever product",
+    quantity: Int = 1,
+    buyer: String = "me, handsome"
+): PurchaseOrder {
+    return PurchaseOrder(
+        product = product,
+        quantity = quantity,
+        buyer = buyer,
+        segments = listOf(Segment(quantity = 1, status = SegmentStatus.NEW, date = LocalDate.now().plusDays(1)))
+    )
 }
