4264a1b77ce9358a3f7127cafca5357fd8c2c97a
Mykola Gurov <mgurov@bol.com>
2019-10-31 23:51:48 +0200

    simple jdbc insert

--- src/main/kotlin/com/github/mgurov/devoxxua19/PurchaseOrderRepository.kt
+++ src/main/kotlin/com/github/mgurov/devoxxua19/PurchaseOrderRepository.kt
@@ -1,11 +1,30 @@
 package com.github.mgurov.devoxxua19
 
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert
 import org.springframework.stereotype.Component
 
 @Component
-class PurchaseOrderRepository {
+class PurchaseOrderRepository(
+    private val jdbcTemplate: NamedParameterJdbcTemplate
+) {
+
+    val insert = SimpleJdbcInsert(jdbcTemplate.jdbcTemplate)
+        .withTableName("purchase_orders")
+        .usingColumns("product", "quantity", "buyer", "segments")
+        .usingGeneratedKeyColumns("id")
+
     fun save(po: PurchaseOrder): Long {
-        TODO()
+        val id = insert.executeAndReturnKey(
+            mapOf(
+                "product" to po.product,
+                "quantity" to po.quantity,
+                "buyer" to po.quantity,
+                "segments" to "[]"
+            )
+        )
+
+        return id.toLong()
     }
 
     fun load(id: Long): PurchaseOrder? {
