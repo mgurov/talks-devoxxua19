636a1af203b1123963fd00bac947a857b06256ad
Mykola Gurov <mgurov@bol.com>
2019-10-31 09:40:46 +0100

    https://start.spring.io/#!type=maven-project&language=kotlin&platformVersion=2.2.0.RELEASE&packaging=jar&jvmVersion=1.8&groupId=com.github.mgurov&artifactId=devoxxua19&name=devoxxua19&description=devoxx%20ua%202019%20demo&packageName=com.github.mgurov.devoxxua19&dependencies=data-jdbc,flyway,postgresql

---
+++ src/main/kotlin/com/github/mgurov/devoxxua19/Devoxxua19Application.kt
@@ -0,0 +1,11 @@
+package com.github.mgurov.devoxxua19
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Devoxxua19Application
+
+fun main(args: Array<String>) {
+	runApplication<Devoxxua19Application>(*args)
+}
---
+++ src/test/kotlin/com/github/mgurov/devoxxua19/Devoxxua19ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.mgurov.devoxxua19
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Devoxxua19ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
