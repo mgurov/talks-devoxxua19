45fd36e78366db2b78b53cecea1f954eb40e2e0a
Mykola Gurov <mgurov@bol.com>
2019-10-31 19:50:52 +0200

    buyer dsl test

--- src/test/kotlin/com/github/mgurov/devoxxua19/TestFixtures.kt
+++ src/test/kotlin/com/github/mgurov/devoxxua19/TestFixtures.kt
@@ -1,19 +1,9 @@
 package com.github.mgurov.devoxxua19
 
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.tuple
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
-
-class PurchaseOrderFixtureTests {
-    @Test
-    fun `should default to quantity 1`() {
-        val actual = aPurchaseOrder()
-
-        softly {
-            assertThat(actual.quantity).isEqualTo(1)
-            assertThat(actual.segments.sumQuantity()).isEqualTo(1)
-        }
-    }
-}
 
 fun aPurchaseOrder(
     product: String = "whatever product",
@@ -40,3 +30,44 @@ fun aSegment(
 )
 
 val defaultSegment = Segment(quantity = 1, status = SegmentStatus.NEW, date = LocalDate.now().plusDays(1))
+
+
+class PurchaseOrderFixtureTests {
+    @Test
+    fun `should default to quantity 1`() {
+        val actual = aPurchaseOrder()
+
+        softly {
+            assertThat(actual.quantity).isEqualTo(1)
+            assertThat(actual.segments.sumQuantity()).isEqualTo(1)
+        }
+    }
+
+    @Test
+    fun `dsl support`() {
+        val po: PurchaseOrder = aPurchaseOrderDsl {
+
+            buyer = "buyer name"
+
+//            aSegmentDsl {
+//                status = SegmentStatus.CANCELLED
+//                quantity = 2
+//            }
+//            aSegmentDsl {
+//                status = SegmentStatus.CONFIRMED
+//                quantity = 3
+//            }
+        }
+
+        //TODO: revisit
+        softly {
+            assertThat(po.buyer).isEqualTo("buyer name")
+
+            assertThat(po.segments).extracting("status", "quantity")
+                .containsExactly(
+                    tuple(SegmentStatus.CANCELLED, 2),
+                    tuple(SegmentStatus.CONFIRMED, 3)
+                )
+        }
+    }
+}
