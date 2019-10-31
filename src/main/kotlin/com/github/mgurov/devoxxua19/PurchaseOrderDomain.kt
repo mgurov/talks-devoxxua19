0e5ba3e2108bf74ad46ee1df79eab4917f0a6808
Mykola Gurov <mgurov@bol.com>
2019-10-31 13:23:27 +0200

    AssertJ contains exactly

--- src/main/kotlin/com/github/mgurov/devoxxua19/PurchaseOrderDomain.kt
+++ src/main/kotlin/com/github/mgurov/devoxxua19/PurchaseOrderDomain.kt
@@ -1,6 +1,6 @@
 package com.github.mgurov.devoxxua19
 
-class PurchaseOrder(
+data class PurchaseOrder(
     val product: String,
     val quantity: Int,
     val buyer: String
--- src/test/kotlin/com/github/mgurov/devoxxua19/BusinessLogicTest.kt
+++ src/test/kotlin/com/github/mgurov/devoxxua19/BusinessLogicTest.kt
@@ -1,21 +1,27 @@
 package com.github.mgurov.devoxxua19
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BusinessLogicTest {
     @Test
     fun `should select by buyer`() {
-        val given = listOf(
-            PurchaseOrder("product A", 1, "me"),
-            PurchaseOrder("product B", 2, "me"),
-            PurchaseOrder("product A", 2, "someone else")
-        )
+        val expected1 = PurchaseOrder("product A", 1, "me")
+        val expected2 = PurchaseOrder("product B", 2, "me")
+        val unexpected1 = PurchaseOrder("product A", 2, "someone else")
 
         //when
-        val actual = BusinessLogic.selectByBuyer(given, "me")
-        //then
+        val actual = BusinessLogic.selectByBuyer(
+            listOf(
+                expected1,
+                expected2,
+                unexpected1
+            ), "me")
 
-        assertEquals(2, actual.size)
+        //then
+        assertThat(actual).containsExactly(
+            expected1,
+            expected2
+        )
     }
 }
