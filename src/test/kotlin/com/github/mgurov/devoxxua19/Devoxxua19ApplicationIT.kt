fadb379e77f54c14268604128547081729efbdfc
Mykola Gurov <mgurov@bol.com>
2019-10-31 12:25:26 +0200

    test that touches Spring Context is an integration test in my universe

--- src/test/kotlin/com/github/mgurov/devoxxua19/Devoxxua19ApplicationIT.kt
+++ src/test/kotlin/com/github/mgurov/devoxxua19/Devoxxua19ApplicationIT.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class Devoxxua19ApplicationTests {
+class Devoxxua19ApplicationIT {
 
 	@Test
 	fun contextLoads() {
