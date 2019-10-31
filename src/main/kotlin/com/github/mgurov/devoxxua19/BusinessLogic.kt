ec16266bae8917368efd7748f9c1f6ff6839f2f6
Mykola Gurov <mgurov@bol.com>
2019-10-31 13:02:11 +0200

    Business Logic interface

---
+++ src/main/kotlin/com/github/mgurov/devoxxua19/BusinessLogic.kt
@@ -0,0 +1,8 @@
+package com.github.mgurov.devoxxua19
+
+object BusinessLogic {
+    fun selectByBuyer(orders: List<PurchaseOrder>, buyerName: String): List<PurchaseOrder> {
+        TODO("TDD in action!")
+    }
+}
+
