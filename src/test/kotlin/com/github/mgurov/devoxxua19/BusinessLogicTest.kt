dbe66eff50d0fa76be223bdeffe062f6a5e93052
Mykola Gurov <mgurov@bol.com>
2019-10-31 13:40:47 +0200

    a simple helper function

--- src/test/kotlin/com/github/mgurov/devoxxua19/BusinessLogicTest.kt
+++ src/test/kotlin/com/github/mgurov/devoxxua19/BusinessLogicTest.kt
@@ -24,4 +24,33 @@ class BusinessLogicTest {
             expected2
         )
     }
+
+    @Test
+    fun `should select by buyer - helper functions`() {
+        val expected1 = aPurchaseOrder("me")
+        val expected2 = aPurchaseOrder("me")
+        val unexpected1 = aPurchaseOrder("someone else")
+
+        //when
+        val actual = BusinessLogic.selectByBuyer(
+            listOf(
+                expected1,
+                expected2,
+                unexpected1
+            ), "me")
+
+        //then
+        assertThat(actual).containsExactlyInAnyOrder(
+            expected1,
+            expected2
+        )
+    }
+
+    fun aPurchaseOrder(buyer: String): PurchaseOrder {
+        return PurchaseOrder(
+            product = "whatever product",
+            quantity = 1,
+            buyer = buyer
+        )
+    }
 }
