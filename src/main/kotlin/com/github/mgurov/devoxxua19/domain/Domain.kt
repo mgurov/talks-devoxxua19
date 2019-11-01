1a89f8575eac3bc3213d309b69957c8a7afebd94
Mykola Gurov <mgurov@bol.com>
2019-11-01 22:30:25 +0200

    Domain: Segment

--- src/main/kotlin/com/github/mgurov/devoxxua19/domain/Domain.kt
+++ src/main/kotlin/com/github/mgurov/devoxxua19/domain/Domain.kt
@@ -1,7 +1,20 @@
 package com.github.mgurov.devoxxua19.domain
 
+import java.time.LocalDate
+
 data class PurchaseOrder(
     val productCode: String,
     val quantity: Int,
-    val buyer: String
+    val buyer: String,
+    val segments: List<Segment>
 )
+
+data class Segment(
+    val status: SegmentStatus,
+    val quantity: Int,
+    val expectedDate: LocalDate
+)
+
+enum class SegmentStatus {
+    NEW, CONFIRMED, RECEIVED, CANCELLED
+}
