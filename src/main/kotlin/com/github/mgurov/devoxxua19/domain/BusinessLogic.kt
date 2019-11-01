0809ef22bbd39fedadc5f947d10b08c993c8a877
Mykola Gurov <mgurov@bol.com>
2019-11-01 21:59:21 +0200

    Business Logic: select by buyer walking skeleton

---
+++ src/main/kotlin/com/github/mgurov/devoxxua19/domain/BusinessLogic.kt
@@ -0,0 +1,7 @@
+package com.github.mgurov.devoxxua19.domain
+
+object BusinessLogic {
+    fun selectByBuyer(orders: List<PurchaseOrder>, buyer: String): List<PurchaseOrder> {
+        TODO()
+    }
+}
